5a635b05ee19f231a32be06bb9d4d208073051ff
olid901 <sinnloseunddummemail@gmail.com>
2020-11-13 16:28:47 +0100

    Added simple feature to list all Cocktails

--- app/src/main/java/com/example/myapplication/Cocktail.java
+++ app/src/main/java/com/example/myapplication/Cocktail.java
@@ -2,10 +2,27 @@ package com.example.myapplication;
 
 import java.util.ArrayList;
 
+import androidx.annotation.NonNull;
+
 public class Cocktail {
     private int ID;
-    private String StrDrink, category;
-    private ArrayList<String> Ingerients, Measures;
+    private String StrDrink;
+    private String Img_Url;
+    //private ArrayList<String> Ingredients, Measures;
 
+    public Cocktail(int ID, String StrDrink, String Img_Url){
+        this.ID = ID;
+        this.StrDrink = StrDrink;
+        this.Img_Url= Img_Url;
+        //this.category = category;
+        //this.Ingredients = Ingredients;
+        //this.Measures = Measures;
+    }
 
+    @NonNull
+    @Override
+    public String toString() {
+        return("Cocktail information: \n"+"ID = "+ID+"\nName = "+StrDrink+"\n");
+        //return super.toString();
+    }
 }
