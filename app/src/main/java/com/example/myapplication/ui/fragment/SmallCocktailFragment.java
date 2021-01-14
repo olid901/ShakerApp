b3e999a4dd2c0a6ad52a3592b094195ce4b09146
Christian Schuhmann <christian8246q@gmail.com>
2021-01-14 19:31:19 +0100

    Achtung! Refactoring voraus!

---
+++ app/src/main/java/com/example/myapplication/ui/fragment/SmallCocktailFragment.java
@@ -0,0 +1,34 @@
+package com.example.myapplication.ui.fragment;
+
+import com.example.myapplication.Cocktail;
+import com.example.myapplication.R;
+import com.example.myapplication.ui.adapter.CocktailRVAdapter;
+import com.example.myapplication.ui.adapter.SmallCocktailRVAdapter;
+
+import java.util.List;
+
+/**
+ * Dieses Fragment zeigt Cocktails klein an, was für größere Listen praktischer wird
+ */
+public class SmallCocktailFragment extends CocktailFragment {
+
+    @Override
+    int getCurrentFragmentID() {
+        return R.layout.fragment_all_cocktails;
+    }
+
+    @Override
+    int getCurrentRecViewID() {
+        return R.id.small_cocktail_rv;
+    }
+
+    @Override
+    String getCocktailListURL() {
+        return "https://www.thecocktaildb.com/api/json/v2/***REMOVED***/filter.php?a=Alcoholic";
+    }
+
+    @Override
+    CocktailRVAdapter getAdapter(List<Cocktail> cocktailList) {
+        return new SmallCocktailRVAdapter(getContext(), cocktailList);
+    }
+}
