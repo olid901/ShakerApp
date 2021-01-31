f5c3954a76412deb991b5b7715ae00941b8c57ef
LeanderCP <leander.c.prange@gmail.com>
2021-01-31 22:11:20 +0100

    Share Button, der noch nicht Funktioniert

--- app/src/main/java/com/example/myapplication/ui/fragment/BigCocktailFragment.java
+++ app/src/main/java/com/example/myapplication/ui/fragment/BigCocktailFragment.java
@@ -1,5 +1,14 @@
 package com.example.myapplication.ui.fragment;
 
+import android.content.Intent;
+import android.media.Image;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
 import com.example.myapplication.Cocktail;
 import com.example.myapplication.Network;
 import com.example.myapplication.R;
@@ -16,12 +25,54 @@ import java.util.LinkedHashMap;
  */
 public class BigCocktailFragment extends CocktailFragment {
 
+ImageButton shareButton;
+
     @Override
     public void fetchAllCocktails() {
         String apiURL = "https://www.thecocktaildb.com/api/json/v2/9973533/popular.php";
         LinkedHashMap<Integer, Cocktail> cocktailMap = new LinkedHashMap<>();
         adapter.setCocktailList(cocktailMap);
         Network.loadCocktails(apiURL, cocktailMap, adapter);
+
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        Log.wtf("Bitte Helfen sie mir", "Ich bin in Gefahr");
+
+        View view =  super.onCreateView(inflater, container, savedInstanceState);
+
+
+
+        ImageButton sharingButton = view.findViewById(R.id.big_cocktail_interaction_share);
+
+        sharingButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                shareIntent();
+
+            }
+
+        });
+
+        return view;
+
+    }
+
+
+
+    private void shareIntent() {
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, "Have a look at this nice cocktail:"+"LINK");
+        sendIntent.setType("text/plain");
+        Intent shareIntent = Intent.createChooser(sendIntent, null);
+        startActivity(shareIntent);
     }
 
     @Override
