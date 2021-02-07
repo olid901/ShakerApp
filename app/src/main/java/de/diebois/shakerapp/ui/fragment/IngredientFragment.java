5ccec0a4f1e73c27c941b4454ee43f81b658f37b
Christian Schuhmann <christian8246q@gmail.com>
2021-02-07 15:26:08 +0100

    Ingredient-Fragmente und RVAdapter aufgeräumt

--- app/src/main/java/de/diebois/shakerapp/ui/fragment/IngredientFragment.java
+++ app/src/main/java/de/diebois/shakerapp/ui/fragment/IngredientFragment.java
@@ -1,69 +1,97 @@
 package de.diebois.shakerapp.ui.fragment;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
-import de.diebois.shakerapp.ui.CocktailClickListener;
-import de.diebois.shakerapp.ui.CocktailDetailsActivity;
+import de.diebois.shakerapp.Ingredient;
+import de.diebois.shakerapp.Network;
+import de.diebois.shakerapp.R;
 import de.diebois.shakerapp.ui.adapter.IngredientRVAdapter;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-/**
- * Allgemeine Cocktail-Fragment-Klasse, da wir vermutlich mehrere
- * Klassen brauchen die alle mehr oder weniger das gleiche machen
- * TODO: Rausfinden, wie ich die Cocktail-Fragment generischer machen kann
- * Vor allem in Hinsicht auf den jeweiligen RV-Adapter
- */
-public abstract class IngredientFragment extends Fragment implements CocktailClickListener {
+import java.util.LinkedHashMap;
 
-    protected IngredientRVAdapter adapter;
+/**
+ * Klasse für das Ingredient-Fragment
+ */
+public class IngredientFragment extends Fragment {
+
+    private IngredientRVAdapter adapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
-    /**
-     * Was getan werden soll, wenn auf ein jeweiliges Cocktail-Item in einer Liste geklickt wird
-     * -> Cocktail öffnen und Zeug wie Zutaten in Großansicht darstellen
-     */
-    @Override
-    public void onItemClick(View view, int position) {
-        Context context = getContext();
-        Intent intent = new Intent(context, CocktailDetailsActivity.class);
-
-        // Wir weisen einem Cocktail direkt das Cocktail-Objekt zu, so haben wir
-        // direkt die Cocktail-Daten da und sparen uns die Netzwerkabfrage
-       // CocktailDetailsActivity.cocktail = adapter.getItem(position);
-        System.out.println("Ingredient has been clicked!");
-        context.startActivity(intent);
-    }
+//    /**
+//     * Was getan werden soll, wenn auf ein jeweiliges Cocktail-Item in einer Liste geklickt wird
+//     * -> Cocktail öffnen und Zeug wie Zutaten in Großansicht darstellen
+//     */
+//    @Override
+//    public void onItemClick(View view, int position) {
+//        Context context = getContext();
+//        Intent intent = new Intent(context, CocktailDetailsActivity.class);
+//
+//        // Wir weisen einem Cocktail direkt das Cocktail-Objekt zu, so haben wir
+//        // direkt die Cocktail-Daten da und sparen uns die Netzwerkabfrage
+//       // CocktailDetailsActivity.cocktail = adapter.getItem(position);
+//        System.out.println("Ingredient has been clicked!");
+//        context.startActivity(intent);
+//    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View view = inflater.inflate(getCurrentFragmentID(), container, false);
+        View view = inflater.inflate(R.layout.fragment_all_ingredients, container, false);
 
-        RecyclerView recyclerView = view.findViewById(getCurrentRecViewID());
+        RecyclerView recyclerView = view.findViewById(R.id.small_ingredient_rv);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
-        adapter = createAdapter();
+        adapter = new IngredientRVAdapter(getContext());
         recyclerView.setAdapter(adapter);
-        adapter.setClickListener(this);
 
         fetchAllIngredients();
+
+        EditText ingredientSearch = view.findViewById(R.id.ingredient_search_text);
+        ingredientSearch.addTextChangedListener(new TextWatcher() {
+            @Override // Brauchen wir nicht
+            public void onTextChanged(CharSequence s, int start, int before, int count) { }
+
+            @Override // Brauchen wir nicht
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s.toString().length() == 0)
+                    fetchAllIngredients();
+                else
+                    filterIngredients(s.toString());
+            }
+        });
         return view;
     }
 
-    abstract public void fetchAllIngredients();
-    abstract int getCurrentFragmentID();
-    abstract int getCurrentRecViewID();
-    abstract IngredientRVAdapter createAdapter();
+    public void fetchAllIngredients() {
+        String apiURL = "https://www.thecocktaildb.com/api/json/v2/9973533/list.php?i=list";
+        LinkedHashMap<String, Ingredient> ingredientMap = new LinkedHashMap<>();
+        adapter.setIngredientList(ingredientMap);
+        Network.loadIngredients(apiURL, null, ingredientMap, adapter);
+    }
+
+    // Standard-Implementierung für eine allgemeine Cocktail-Suche
+    // TODO Eigentlich kann man das auch mit der fetchAllIngredients-Methode kombinieren
+    private void filterIngredients(String filter) {
+        String IngredientURL = "https://www.thecocktaildb.com/api/json/v2/9973533/list.php?i=list";
+        LinkedHashMap<String, Ingredient> ingredientMap = new LinkedHashMap<>();
+        adapter.setIngredientList(ingredientMap);
+        Network.loadIngredients(IngredientURL, filter, ingredientMap, adapter);
+    }
 }
